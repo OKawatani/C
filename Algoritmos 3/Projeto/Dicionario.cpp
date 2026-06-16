0c3f8a800ca9a7b2b16558ef27a25f6e30e466c6
Eduardo <eduardokawatani@gmail.com>
2026-06-16 16:02:54 -0300

    codigo do projeto finalizado

--- Algoritmos 3/Projeto/Dicionario.cpp
+++ Algoritmos 3/Projeto/Dicionario.cpp
@@ -8,8 +8,10 @@
 #include <cstdlib>
 #include <list>
 #include <string>
+#include <cmath>
 using namespace std;
 
+// Armazenamento de dados 
 typedef struct treenode* treenodeptr;
 
 struct Aresta {
@@ -32,6 +34,9 @@ struct treenode{
 
 list<No> grafo;
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 //Funções de árvore
 
 void tInsert(treenodeptr &p, No x){
@@ -50,11 +55,70 @@ void tInsert(treenodeptr &p, No x){
     }
 }
 
-//---------------------------------------------------------------------
+treenodeptr tPointSmaller(treenodeptr &p){
+    treenodeptr t = p;
+
+    if(p->left == NULL){
+        p = p->right;
+        return t;
+    }
+    else{
+        return tPointSmaller(p->left);
+    }
+}
+
+bool tRemove(treenodeptr &p, string x){
+        
+    treenodeptr t;
+        
+    if(p == NULL)return false;
+        
+    if(x == p->info.palavraFicticia){
+            
+        t = p;
+            
+        if(p->left == NULL){
+            p = p->right;
+        }
+            
+        else if(p->right == NULL){
+            p = p->left;
+        }
+            
+        else{
+            t = tPointSmaller(p->right);
+            p->info = t->info;
+        }
+
+        delete t;
+        return true;
+    }
+
+    else if(x < p->info.palavraFicticia) return tRemove(p->left,x);
+    else return tRemove(p->right,x);
+}
+
+void percorrerArvore(treenodeptr p) {
+    if (p != NULL) {
+        percorrerArvore(p->left);
+        cout << "Palavra Ficticia: "<<  p->info.palavraFicticia << endl;
+        cout << "Palavra em Portugues: " << p->info.palavraPortugues << endl;
+        cout << "Significado: " << p->info.palavraSignificado << endl;
+        cout << "------------------------" << endl;
+        percorrerArvore(p->right);
+    }
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Funções principais 
 
 void cadastrarPalavra(treenodeptr &p) {
+
     No novo;
-    
+    char outro;
 
     cout << "Palavra Ficticia: "; cin >> novo.palavraFicticia;
     cout << "Traducao (Portugues): "; cin >> novo.palavraPortugues;
@@ -63,8 +127,20 @@ void cadastrarPalavra(treenodeptr &p) {
 
     Aresta relacao;
     relacao.destino = novo.palavraSignificado;
-    
+
     novo.adjacencias.push_back(relacao);
+
+    do {
+
+        cout << "Quer digitar outro significado? (s/n)";
+        cin >> outro;
+
+        if (outro == 's'){
+            Aresta relacao2;
+            cin >> relacao2.destino;
+            novo.adjacencias.push_back(relacao2);
+        }
+    } while (outro == 's');
 
     grafo.push_back(novo);
 
@@ -82,11 +158,11 @@ void listarSignificados() {
     list<No>::iterator it;
     for (it = grafo.begin(); it != grafo.end(); ++it) {
         if (it->palavraFicticia == busca) {
-            cout << "Significado de: " << busca;
-            
+            cout << "Significado de "<< busca << ": ";
+
             list<Aresta>::iterator ita;
             for (ita = it->adjacencias.begin(); ita != it->adjacencias.end(); ++ita) {
-                cout << ": " << ita->destino << "\n";
+                cout << "\n" << ita->destino;
             }
             return;
         }
@@ -95,51 +171,67 @@ void listarSignificados() {
 }
 
 void listarSinonimos() {
+
     string busca;
+
     cout << "Digite a palavra para buscar sinonimos: ";
     cin >> busca;
 
-    string significadoBase = "";
-    bool encontrou = false;
+    No* palavraBase = NULL;
 
     list<No>::iterator it;
 
-    for (it = grafo.begin(); it != grafo.end(); ++it) {
-        if (it->palavraFicticia == busca) {
-            significadoBase = it->palavraSignificado; 
-            encontrou = true;
+    // encontra palavra buscada
+    for(it = grafo.begin(); it != grafo.end(); ++it){
+
+        if(it->palavraFicticia == busca){
+
+            palavraBase = &(*it);
+
             break;
         }
     }
 
-    if (!encontrou) {
+    if(palavraBase == NULL){
+
         cout << "Palavra nao encontrada.\n";
         return;
     }
 
     cout << "Sinonimos de " << busca << ":\n";
 
-    bool temSinonimo = false;
+    bool encontrou = false;
 
-    for (it = grafo.begin(); it != grafo.end(); ++it) {
-        if (it->palavraFicticia != busca &&
-            it->palavraSignificado == significadoBase) {
+    // percorre todas as palavras
+    for(it = grafo.begin(); it != grafo.end(); ++it){
 
+        if(it->palavraFicticia == busca)
+            continue;
+
+        bool igual = false;
+
+        list<Aresta>::iterator s1;
+        list<Aresta>::iterator s2;
+
+        // compara TODOS os significados
+        for(s1 = palavraBase->adjacencias.begin(); s1 != palavraBase->adjacencias.end(); ++s1){
+
+            for(s2 = it->adjacencias.begin(); s2 != it->adjacencias.end(); ++s2){
+
+                if(s1->destino == s2->destino){
+                    igual = true;
+                }
+            }
+        }
+
+        if(igual){
             cout << "- " << it->palavraFicticia << endl;
-            temSinonimo = true;
+            encontrou = true;
         }
     }
 
-    if (!temSinonimo) {
+    if(!encontrou){
         cout << "Nenhum sinonimo encontrado.\n";
-    }
-}
-
-void percorrerArvore(treenodeptr p) {
-    if (p != NULL) {
-        percorrerArvore(p->left);
-        cout << p->info.palavraFicticia << endl;
-        percorrerArvore(p->right);
     }
 }
 
@@ -152,16 +244,115 @@ void listarOrdemAlfabetica(treenodeptr p) {
 }
 
 void listarPorTamanho() {
-    cout << "Funcionalidade em construcao\n";
+    
+    if (grafo.empty()) {
+        cout << "Cadastre uma palavra antes" << endl;
+        return;
+    }
+
+    list<No> listaOrdenada = grafo;
+    bool trocou;
+
+    do {
+        trocou = false;
+        list<No>::iterator it = listaOrdenada.begin();
+        list<No>::iterator proximo = it;
+        proximo++;
+
+        while (proximo != listaOrdenada.end()) {
+            if (it->palavraFicticia.length() > proximo->palavraFicticia.length()) {
+                
+                No x = *it;
+                *it = *proximo; 
+                *proximo = x;
+                trocou = true;}
+                it++; 
+                proximo++;
+            
+            }
+    } while (trocou);
+  
+    list<No>::iterator ordem;
+    for (ordem = listaOrdenada.begin(); ordem != listaOrdenada.end(); ordem++) {
+      cout << ordem->palavraFicticia << ", ";
+    }
 }
 
-void removerPalavra() {
-    cout << "Funcionalidade em construcao\n";
+void removerPalavra(treenodeptr &p){
+
+    string palavra;
+
+    cout << "Digite a palavra ficticia que deseja remover: ";
+    cin >> palavra;
+
+    bool result = tRemove(p, palavra);
+
+    if(result){
+
+        list<No>::iterator it;
+
+        for(it = grafo.begin();it != grafo.end();){
+
+            if(it->palavraFicticia == palavra){
+                it = grafo.erase(it);
+            }
+            else{
+                ++it;
+            }
+        }
+
+        cout << "Palavra removida com sucesso\n";
+    }
+
+    else{
+
+        cout << "Palavra nao encontrada\n";
+
+    }
 }
 
 void calcularSimilaridade() {
-    cout << "Funcionalidade em construcao\n";
+    string palavra1, palavra2;
+
+    cout << "Digite a primeira palavra: ";
+    cin >> palavra1;
+
+    cout << "Digite a segunda palavra: ";
+    cin >> palavra2;
+
+    No* p1 = NULL;
+    No* p2 = NULL;
+
+    list<No>::iterator it;
+
+    for (it = grafo.begin(); it != grafo.end(); ++it) {
+        if (it->palavraFicticia == palavra1) {
+            p1 = &(*it);
+        }
+
+        if (it->palavraFicticia == palavra2) {
+            p2 = &(*it);
+        }
+    }
+
+    if (p1 == NULL || p2 == NULL) {
+        cout << "Palavra nao encontrada.\n";
+        return;
+    }
+
+    float dx = p2->x - p1->x;
+    float dy = p2->y - p1->y;
+    float dz = p2->z - p1->z;
+
+    float distancia = sqrt((dx * dx) + (dy * dy) + (dz * dz));
+
+    cout << "Similaridade de "
+         << palavra1 << " e "
+         << palavra2 << " = "
+         << distancia << endl;
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 int main() {
 	
@@ -197,7 +388,7 @@ int main() {
             case 3: listarSinonimos(); break;
             case 4: listarOrdemAlfabetica(p); break;
             case 5: listarPorTamanho(); break;
-            case 6: removerPalavra(); break;
+            case 6: removerPalavra(p); break;
             case 7: calcularSimilaridade(); break;
             case 0: cout << "Saindo...\n"; break;
             default: cout << "Opcao invalida!\n";
